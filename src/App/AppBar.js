b138f9ad2b30f4004b372aa51a9fadacc85e310b
Dalton Okechukwu <okechukwu@whitewhale.ai>
2020-11-05 11:11:52 -0700

    some styling for AppBar

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -1,18 +1,39 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
 
 const Bar = styled.div`
     display: grid;
     grid-template-columns: 180px auto 100px 100px;
+    margin-bottom: 40px;
 `
+
+const Logo = styled.div`
+    font-size: 1.5em;
+`
+
+const ControlButtonElem = styled.div`
+    cursor: pointer;
+    ${props => props.active && css`
+        color: #0cff00;
+    `}
+`
+const toProperCase = (lower) => {
+    return lower.charAt(0).toUpperCase() + lower.substr(1);
+}
+
+const ControlButton = ({name, active}) => {
+    return (
+        <ControlButtonElem active={active}>{toProperCase(name)}</ControlButtonElem>
+    )
+}
 
 const AppBar = () => {
     return ( 
     <Bar>
-        <div>CryptoDash</div>
+        <Logo>CryptoDash</Logo>
         <div/>
-        <div>Dashboard</div>
-        <div>Settings</div>
+        <ControlButton active name="dashboard"/>
+        <ControlButton name="settings"/>
     </Bar> 
     );
 }

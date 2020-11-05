6812831258c1067e2f5dc1f19104e4e340b7259a
Dalton Okechukwu <okechukwu@whitewhale.ai>
2020-11-04 20:01:18 -0700

    minor refactor, installed styled components

--- src/App/index.js
+++ src/App/index.js
@@ -1,25 +1,16 @@
-import logo from '../logo.svg';
+import React, {Component} from 'react';
+import styled, {css} from 'styled-components'
+import WelcomeMessage from './WelcomeMessage';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to CryptoDash
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <WelcomeMessage/>
+      </div>
+    );
+  }
 }
 
 export default App;
